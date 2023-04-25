ef8cf4389462f0fed71fd1767701ecca87ded863
rpcmoreira <rpcardoso2@outlook.com>
2023-04-25 17:01:26 +0100

    inicio views e some db changes

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="{{ url('/') }}">Sell.Me</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse"
+  data-target="#navbarSupportedContent"><span class="navbar-toggler-icon"></span></button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav ml-auto">
+
+      @guest
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" role="button"
+      data-toggle="dropdown" aria-expanded="false">Account</a>
+        <ul class="dropdown-menu dropdown-menu-right">
+        @if (Route::has('login'))
+        <a class="dropdown-item" href="{{ route('login') }}">{{ ('Login') }}</a>
+        @endif
+        @if (Route::has('register'))
+        <a class="dropdown-item" href="{{ route('register') }}">
+          {{ __('Register') }}
+        </a>
+        </ul>
+      </li>
+      @endif
+      @endguest
+    </ul>
+  </div>
+
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+    crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>@yield('Name', 'Best Page for buying stuff')</title>
+</head>
+
+<body>
+    @include('includes.header')
+        @yield('content', '')
+    @include('includes.footer')
+
+    <!-- Optional JavaScript -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+</body>
+</html>
