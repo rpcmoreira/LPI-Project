8c94d336ba99873cb8bc8048d3b0c00eff859ca4
rpcmoreira <rpcardoso2@outlook.com>
2023-05-11 12:30:56 +0100

    header + rotas

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,58 +6,75 @@
   <button type="button" data-toggle="collapse" data-target="#navbarNav" class="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Nav">
     <span class="navbar-toggler-icon"></span>
   </button>
-  @if(Auth::user() != null)
+  @if(Auth::user() != null && Auth::user()->tipo_id == 6)
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item ">
-        <a href="#" class="nav-link text-light">Features</a>
-      </li>
       <li class="nav-item">
-        <a href="#" class="nav-link text-light">Price</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulários</a>
-        <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="{{ route('q250') }}">{{ __('Q250') }}</a>
-          <a class="dropdown-item" href="{{ route('q251') }}">{{ __('Q251 - Submissão Parecer') }}</a>
-          <a class="dropdown-item" href="{{ route('q252') }}">{{ __('Q252 - Consentimento Informado') }}</a>
-          <a class="dropdown-item" href="{{ route('q381') }}">{{ __('Q381 - Síntese de Resultados') }}</a>
-        </div>
-      </li>
-    </ul>
-    @endif
-
-    <ul class="navbar-nav ml-auto">
-      @guest
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
-        <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
-          @if (Route::has('login'))
-          <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-          @endif
-
-          @if (Route::has('register'))
-          <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
-          @endif
-          @else
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-light ml-auto" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          {{ Auth::user()->nome }}
+        <a class="nav-link text-light" href="{{ route('dashboard') }}">
+          <span data-feather="home"></span>
+          Home <span class="sr-only">(current)</span>
         </a>
-        <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-          </a>
-
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-          </form>
-        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span data-feather="file"></span>Formulários</a>
+          <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="{{ route('q250') }}">{{ __('Q250') }}</a>
+            <a class="dropdown-item" href="{{ route('q251') }}">{{ __('Q251 - Submissão Parecer') }}</a>
+            <a class="dropdown-item" href="{{ route('q252') }}">{{ __('Q252 - Consentimento Informado') }}</a>
+            <a class="dropdown-item" href="{{ route('q381') }}">{{ __('Q381 - Síntese de Resultados') }}</a>
+          </div>
       </li>
     </ul>
-    @endguest
-  </div>
+    @elseif(Auth::user() != null && Auth::user()->tipo_id == 5)
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link text-light" href="{{ route('dashboard') }}">
+            <span data-feather="bar-chart-2"></span>
+            Dashboard <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="{{ route('projectlist') }}">
+            <span data-feather="file"></span>
+            Lista de Projetos
+          </a>
+        </li>
+      </ul>
+      @else
+      @endif
+
+      <ul class="navbar-nav ml-auto">
+        @guest
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+          <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
+            @if (Route::has('login'))
+            <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+            @endif
+
+            @if (Route::has('register'))
+            <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+            @endif
+            @else
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light ml-auto" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{ Auth::user()->nome }}
+          </a>
+          <div class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+          </div>
+        </li>
+      </ul>
+      @endguest
+    </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -35,35 +35,35 @@ Route::get('/logged', [ProjectController::class, 'logged']);
 Route::get('/', [ProjectController::class, 'start']);
 Route::get('/home', [ProjectController::class, 'dashboard'])->name('home')->middleware('auth');
 
-Route::get('/dashboard', [ProjectController::class, 'dashboard'])->name('dashboard')->middleware('auth');;
+Route::get('/dashboard', [ProjectController::class, 'dashboard'])->name('dashboard')->middleware('auth');
 
-Route::get('/projectlist', [ProjectController::class, 'projectList'])->name('projectlist')->middleware('auth');;
+Route::get('/projectlist', [ProjectController::class, 'projectList'])->name('projectlist')->middleware('auth');
 
-Route::get('/projetoInfo', [ProjectController::class, 'projetoInfo'])->name('projeto_info')->middleware('auth');;
-
-
+Route::get('/projetoInfo', [ProjectController::class, 'projetoInfo'])->name('projeto_info')->middleware('auth');
 
 
-Route::get('/q250', [ProjectController::class, 'q250'])->name('q250')->middleware('auth');;
-Route::post('/q250_form', [ProjectController::class, 'q250_form'])->name('q250_form')->middleware('auth');;
+
+
+Route::get('/q250', [ProjectController::class, 'q250'])->name('q250')->middleware('auth');
+Route::post('/q250_form', [ProjectController::class, 'q250_form'])->name('q250_form')->middleware('auth');
 
 //rotas teste - apagar no final
-Route::get('/download250',[ProjetoController::class, 'get250'])->middleware('auth');;
-Route::get('/download251',[ProjetoController::class, 'get251'])->middleware('auth');;
-Route::get('/download252',[ProjetoController::class, 'get252'])->middleware('auth');;
+Route::get('/download250',[ProjetoController::class, 'get250'])->middleware('auth');
+Route::get('/download251',[ProjetoController::class, 'get251'])->middleware('auth');
+Route::get('/download252',[ProjetoController::class, 'get252'])->middleware('auth');
 
-Route::get('/q251', [ProjectController::class, 'q251'])->name('q251')->middleware('auth');;
-Route::post('/q251_form', [ProjectController::class, 'q251_form'])->name('q251_form')->middleware('auth');;
-
-
-Route::get('/q252', [ProjectController::class, 'q252'])->name('q252')->middleware('auth');;
-Route::post('/q252_form', [ProjectController::class, 'q252_form'])->name('q252_form')->middleware('auth');;
+Route::get('/q251', [ProjectController::class, 'q251'])->name('q251')->middleware('auth');
+Route::post('/q251_form', [ProjectController::class, 'q251_form'])->name('q251_form')->middleware('auth');
 
 
-Route::get('/q381', [ProjetoController::class, 'q381'])->name('q381')->middleware('auth');;
-Route::post('/q381_form', [ProjetoController::class, 'q381_form'])->name('q381_form')->middleware('auth');;
+Route::get('/q252', [ProjectController::class, 'q252'])->name('q252')->middleware('auth');
+Route::post('/q252_form', [ProjectController::class, 'q252_form'])->name('q252_form')->middleware('auth');
 
-Route::post('/gerar-pdf-q250', 'App\Http\Controllers\PdfController@generateFilled_q250_Pdf')->name('gerar-pdf-q250')->middleware('auth');;
-Route::post('/gerar-pdf-q251', 'App\Http\Controllers\PdfController@generateFilled_q251_Pdf')->name('gerar-pdf-q251')->middleware('auth');;
-Route::post('/gerar-pdf-q252', 'App\Http\Controllers\PdfController@generateFilled_q252_Pdf')->name('gerar-pdf-q252')->middleware('auth');;
+
+Route::get('/q381', [ProjetoController::class, 'q381'])->name('q381')->middleware('auth');
+Route::post('/q381_form', [ProjetoController::class, 'q381_form'])->name('q381_form')->middleware('auth');
+
+Route::post('/gerar-pdf-q250', 'App\Http\Controllers\PdfController@generateFilled_q250_Pdf')->name('gerar-pdf-q250')->middleware('auth');
+Route::post('/gerar-pdf-q251', 'App\Http\Controllers\PdfController@generateFilled_q251_Pdf')->name('gerar-pdf-q251')->middleware('auth');
+Route::post('/gerar-pdf-q252', 'App\Http\Controllers\PdfController@generateFilled_q252_Pdf')->name('gerar-pdf-q252')->middleware('auth');
 
